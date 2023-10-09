531dcb758b5c9ff55d9f63019ec011f1b301b7ab
Liangyiu <ms290498@gmail.com>
2023-10-09 23:17:54 +0200

    adjusted config to vercel url

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,4 +2,4 @@ import { dev } from '$app/environment';
 
 export const title = `Lingling's Dev Adventures`;
 export const description = 'Blog for my development projects and what I learned on the way';
-export const url = dev ? 'http://localhost:5173' : 'https://blog.liangyiu.com';
+export const url = dev ? 'http://localhost:5173' : 'https://liangyiu.vercel.app';
